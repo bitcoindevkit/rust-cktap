ba4c70b6bcfdb5b34a9bc76629acee4499ddbf95
Rubens <r1b2ns@duck.com>
2026-04-15 11:51:25 -0300

    feat: configuring maven local

--- cktap-android/lib/build.gradle.kts
+++ cktap-android/lib/build.gradle.kts
@@ -1,12 +1,17 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SourcesJar
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.dokka")
+    id("com.vanniktech.maven.publish")
 }
 
-group = "com.coinkite.cktap"
+group = "org.bitcoindevkit"
 version = "0.1.0-SNAPSHOT"
 
 android {
@@ -51,4 +56,77 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("org.jetbrains.kotlin:kotlin-test:1.6.10")
     androidTestImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.6.10")
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "cktap-android",
+        version = version.toString()
+    )
+
+    pom {
+        name.set("cktap-android")
+        description.set("Coinkite Tap Protocol language bindings for Android.")
+        url.set("https://github.com/bitcoindevkit/rust-cktap")
+        inceptionYear.set("2025")
+        licenses {
+            license {
+                name.set("APACHE 2.0")
+                url.set("https://github.com/bitcoindevkit/rust-cktap/blob/master/LICENSE-APACHE")
+            }
+            license {
+                name.set("MIT")
+                url.set("https://github.com/bitcoindevkit/rust-cktap/blob/master/LICENSE-MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("cktapdevelopers")
+                name.set("rust-cktap developers")
+                email.set("dev@bitcoindevkit.org")
+            }
+        }
+        scm {
+            url.set("https://github.com/bitcoindevkit/rust-cktap/")
+            connection.set("scm:git:github.com/bitcoindevkit/rust-cktap.git")
+            developerConnection.set("scm:git:ssh://github.com/bitcoindevkit/rust-cktap.git")
+        }
+    }
+
+    configure(
+        AndroidSingleVariantLibrary(
+            javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationHtml"),
+            sourcesJar = SourcesJar.Sources(),
+            variant = "release",
+        )
+    )
+
+    publishToMavenCentral()
+    // Signing is only applied when NOT in localBuild mode AND GPG props are present.
+    // This keeps local development fully functional without any credentials.
+    // To enable signing (for Maven Central release), add to ~/.gradle/gradle.properties:
+    //   signing.gnupg.keyName=<GPG_KEY_ID>
+    //   signing.gnupg.passphrase=<GPG_PASSPHRASE>
+    if (!project.hasProperty("localBuild")
+        && project.findProperty("signing.gnupg.keyName") != null
+    ) {
+        signAllPublications()
+    }
+}
+
+dokka {
+    moduleName.set("cktap-android")
+    moduleVersion.set(version.toString())
+    dokkaSourceSets.main {
+        includes.from("../docs/DOKKA_LANDING.md")
+        sourceLink {
+            localDirectory.set(file("src/main/kotlin"))
+            remoteUrl("https://github.com/bitcoindevkit/rust-cktap")
+            remoteLineSuffix.set("#L")
+        }
+    }
+    pluginsConfiguration.html {
+        footerMessage.set("(c) rust-cktap Developers")
+    }
 }
